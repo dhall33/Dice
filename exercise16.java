6598f92e156ae5059582a15ed5ebc3897c19bd0e
dhall33 <ehall33@student.alamo.edu>
2017-04-04 21:34:58 -0500

    Add files via upload

---
+++ exercise16.java
@@ -0,0 +1,25 @@
+//Daneil Hall
+//April 4 2017
+//ITSE
+//A short program that rolls dice 
+
+
+public class exercise16 {
+    public static void main (String[] argStr) {
+	
+	
+	PairOfDice dice = new PairOfDice();
+	int boxcars = 0;                                // stores number of boxcars
+	final int intRolls = 1000, intBoxes = 12; 
+
+	
+	for (int i = 0; i < intRolls; i++) { //rolls dice
+	    if (dice.roll() == intBoxes) {
+		boxcars++;
+	    }
+	}
+
+	// output the results
+	System.out.println ("Rolled " + intRolls + " pairs of dice with " + boxcars + " boxcars.");
+    }
+}
